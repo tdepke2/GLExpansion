3f6d9757b33e9b30029a270ba28760a050681057
Thomas Depke <tdepke@mymail.mines.edu>
2020-07-11 16:43:46 -0600

    Added keyframe lerp for smoother animations.

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -4,11 +4,11 @@
 Animation::Animation(const string& name, double duration, double ticksPerSecond) : name(name), duration(duration), ticksPerSecond(ticksPerSecond) {}
 
 glm::mat4 Animation::calcChannelTransform(unsigned int channelIndex, double animationTime) const {
-    glm::mat4 translationMtx = glm::translate(glm::mat4(1.0f), _interpolateVec3Keys(channels[channelIndex].translationKeys, animationTime));
-    glm::mat4 rotationMtx = glm::mat4_cast(_interpolateQuatKeys(channels[channelIndex].rotationKeys, animationTime));
-    glm::mat4 scalingMtx = glm::scale(glm::mat4(1.0f), _interpolateVec3Keys(channels[channelIndex].scalingKeys, animationTime));
+    glm::mat4 transform = glm::translate(glm::mat4(1.0f), _interpolateVec3Keys(channels[channelIndex].translationKeys, animationTime));
+    transform *= glm::mat4_cast(_interpolateQuatKeys(channels[channelIndex].rotationKeys, animationTime));
+    transform = glm::scale(transform, _interpolateVec3Keys(channels[channelIndex].scalingKeys, animationTime));
     
-    return translationMtx * rotationMtx * scalingMtx;
+    return transform;
 }
 
 glm::vec3 Animation::_interpolateVec3Keys(const vector<pair<glm::vec3, double>>& keys, double animationTime) const {
@@ -22,11 +22,12 @@ glm::vec3 Animation::_interpolateVec3Keys(const vector<pair<glm::vec3, double>>&
         }
         ++i;
     }
-    if (i == keys.size() - 1) {
+    if (i >= keys.size() - 1) {
         assert(false);
     }
     
-    return keys[i].first;
+    float t = static_cast<float>((animationTime - keys[i].second) / (keys[i + 1].second - keys[i].second));
+    return glm::mix(keys[i].first, keys[i + 1].first, t);
 }
 
 glm::quat Animation::_interpolateQuatKeys(const vector<pair<glm::quat, double>>& keys, double animationTime) const {
@@ -40,9 +41,10 @@ glm::quat Animation::_interpolateQuatKeys(const vector<pair<glm::quat, double>>&
         }
         ++i;
     }
-    if (i == keys.size() - 1) {
+    if (i >= keys.size() - 1) {
         assert(false);
     }
     
-    return keys[i].first;
+    float t = static_cast<float>((animationTime - keys[i].second) / (keys[i + 1].second - keys[i].second));
+    return glm::slerp(keys[i].first, keys[i + 1].first, t);
 }
